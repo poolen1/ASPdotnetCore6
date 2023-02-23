0a9158e67212e192252b00cdb3d4d8113e1f43e2
poolen1 <poolen108@gmail.com>
2023-02-22 17:58:58 -0800

    Add 2 data models
    City.cs
    Country.cs

---
+++ WorldCities/WorldCitiesAPI/Data/Models/City.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldCitiesAPI.Data.Models
+{
+    public class City
+    {
+        #region Properties
+
+        /// <summary>
+        /// Unique id and primary key for this city
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// City name (UTF8)
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// City Latitude
+        /// </summary>
+        [Column(TypeName = "decimal(7,4)")]
+        public decimal Lat { get; set; }
+
+        /// <summary>
+        /// City Longitude
+        /// </summary>
+        [Column(TypeName = "decimal(7,4)")]
+        public decimal Lon { get; set; }
+
+        /// <summary>
+        /// Country Id (foreign key)
+        /// </summary>
+        [ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        /// <summary>
+        /// The country related to this city
+        /// </summary>
+        public Country? Country { get; set; } = null!;
+        #endregion
+    }
+}
---
+++ WorldCities/WorldCitiesAPI/Data/Models/Country.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorldCitiesAPI.Data.Models
+{
+    public class Country
+    {
+        #region Properties
+
+        /// <summary>
+        /// Unique id and primary key for this country
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Country name (UTF8)
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Country code (ISO 3166-1 ALPHA-2 format)
+        /// </summary>
+        public string ISO2 { get; set; } = null!;
+
+        /// <summary>
+        /// Country code (ISO 3166-1 ALPHA-3 format)
+        /// </summary>
+        public string ISO3 { get; set; } = null!;
+
+        #endregion
+
+        #region Navigation Properties
+        /// <summary>
+        /// A collection of all cities related to this country
+        /// </summary>
+        public ICollection<City>? Cities { get; set; } = null!;
+        #endregion
+    }
+}
