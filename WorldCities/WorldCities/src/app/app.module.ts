a310415fcdf49b0801e3becec171f361de70fa55
poolen1 <poolen108@gmail.com>
2023-03-01 13:09:24 -0800

    Cities page w/ pagination

--- WorldCities/WorldCities/src/app/app.module.ts
+++ WorldCities/WorldCities/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CitiesComponent } from './cities/cities.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -25,9 +25,7 @@ import { CitiesComponent } from './cities/cities.component';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatToolbarModule
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- WorldCities/WorldCities/src/app/cities/cities.component.ts
+++ WorldCities/WorldCities/src/app/cities/cities.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 import { City } from './city';
 
@@ -10,15 +12,20 @@ import { City } from './city';
   styleUrls: ['./cities.component.scss']
 })
 export class CitiesComponent implements OnInit {
-  public cities!: City[];
+  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
+  public cities!: MatTableDataSource<City>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
+    console.log("get url: ", environment.baseUrl + 'api/Cities');
     this.http.get<City[]>(environment.baseUrl + 'api/Cities')
       .subscribe(result => {
-          this.cities = result;
+          this.cities = new MatTableDataSource<City>(result);
+          this.cities.paginator = this.paginator;
         },
         error => console.error(error));
   }
