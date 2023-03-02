7deac33658b44d572c44a5a728c53fe20bdc35af
poolen1 <poolen108@gmail.com>
2023-03-02 15:46:53 -0800

    Add city edit form, fix margin bug

--- WorldCities/WorldCities/src/app/app-routing.module.ts
+++ WorldCities/WorldCities/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { CitiesComponent } from './cities/cities.component';
+import { CityEditComponent } from './cities/city-edit.component';
 import { CountriesComponent } from './countries/countries.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'cities', component: CitiesComponent },
+  { path: 'city/:id', component: CityEditComponent },
   { path: 'countries', component: CountriesComponent }
 ];
 
--- WorldCities/WorldCities/src/app/cities/city-edit.component.ts
+++ WorldCities/WorldCities/src/app/cities/city-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { environment } from './../../environments/environment';
+import { City } from './city';
 
 @Component({
   selector: 'app-city-edit',
@@ -7,9 +13,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CityEditComponent implements OnInit {
 
-  constructor() { }
+  title?: string;
+  form!: FormGroup;
+  city?: City;
 
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient) {
   }
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(''),
+      lat: new FormControl(''),
+      lon: new FormControl('')
+    });
+
+    this.loadData();
+  }
+
+  loadData() {
+    var idParam = this.activatedRoute.snapshot.paramMap.get('id');
+    var id = idParam ? + idParam : 0;
+
+    var url = environment.baseUrl + 'api/Cities/' + id;
+    this.http.get<City>(url).subscribe(result => {
+        this.city = result;
+        this.title = "Edit - " + this.city.name;
+
+        this.form.patchValue(this.city);
+      },
+      error => console.error(error));
+  }
+
+  onSubmit() {
+    var city = this.city;
+    if (city) {
+      city.name = this.form.controls['name'].value;
+      city.lat = this.form.controls['lat'].value;
+      city.lon = this.form.controls['lon'].value;
+
+      var url = environment.baseUrl + 'api/Cities/' + city.id;
+      this.http
+        .put<City>(url, city)
+        .subscribe(result => {
+
+          console.log("City " + city!.id + " has been updated.");
+
+          this.router.navigate(['/cities']);
+        }, error => console.error(error));
+    }
+  }
 }
